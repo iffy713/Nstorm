a170ea05a384c01c167b83bc8d7a3699efe4f857
yifan <iffy713@gmail.com>
2022-12-05 02:43:19 -0500

    strating work on react store

---
+++ react-app/src/store/address.js
@@ -0,0 +1,27 @@
+const GET_ALL_ADDRESSES = "address/GET_ALL_ADDRESSES"
+const GET_SINGLE_ADDRESS = "address/GET_SINGLE_ADDRESS"
+
+const actionGetAllAddresses = (addresses) => ({
+    type: GET_ALL_ADDRESSES,
+    addresses
+})
+
+const actionGetSingleAddress = (address) => ({
+    type: GET_SINGLE_ADDRESS,
+    address
+})
+
+// ==============   Thunk   ==================
+
+// -------------- Get all address of current user --------------
+export const thunkGetAllAddresses() => async (dispatch) => {
+    const response = await fetch('/api/current')
+    const data = await response.json()
+    if (response.ok) {
+        dispatch(actionGetAllAddresses())
+    }
+}
+
+
+// ============   Reducer   ==================
+const initialState = { allAddresses: {}, singleAddress:{} }
