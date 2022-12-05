52dbb7b4b3545350084847b7c7f250977855c04f
yifan <iffy713@gmail.com>
2022-12-05 02:29:40 -0500

    address routes all done

--- app/api/address_routes.py
+++ app/api/address_routes.py
@@ -26,40 +26,29 @@ def get_all_addresses():
         output.append(address.to_dict())
     return jsonify({"Addresses":output})
 
+# ============ Get address by id =============
+@address_routes.route('/<int:address_id>')
+def get_address_by_id(address_id):
+    address = Address.query.get(address_id)
+    if not address:
+        return {
+            "message": "Address couldn't found.",
+            "status_code": 404
+        }, 404
+    else:
+        return jsonify(address.to_dict_with_users())
 
 # =========== Get all addresses of current user ===========
 @address_routes.route('/current')
 @login_required
 def get_current_user_addresses():
-    # output = []
-    # real_output = []
-
-    # addresses = Address.query.options(joinedload(Address.users)).all()
-    # print("addresses here",addresses)
-
-    # for address in addresses:
-    #     output.append(address.to_dict_with_users())
-    # print(output)
-
-    # for single_address in output:
-    #     for i in single_address["Users"]:
-    #         print("=========", i)
-    #         if i["id"] == current_user.id:
-    #             real_output.append(i)
-
-    # print("am i correct??????",real_output)
-    # return jsonify(real_output)
-    # return "testing"
+    addresses = current_user.addresses
     output = []
-    addresses = Address.query.options(joinedload(Address.users)).all()
-    # address = User.query.filter()
-    print("addresses here", addresses)
-
     for address in addresses:
-        output.append(address.to_dict_with_users())
+        output.append(address.to_dict())
     return jsonify(output)
 
-# =========== Create an addresses ===========
+# =========== Create a new address ===========
 @address_routes.route('/', methods=['POST'])
 @login_required
 def create_new_address():
@@ -74,20 +63,35 @@ def create_new_address():
             state = form.data['state'],
             zip_code = form.data['zip_code']
         )
-        print("!!!!!!!", current_user.addresses)
         current_user.addresses.append(new_address)
         db.session.commit()
         return new_address.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
-# @address_routes.route('/', methods=['POST'])
-# @login_required
-# def user_update_address():
-#     form = AddressForm()
-#     form['csrf_token'].data = request.cookies['csrf_token']
+# ============ Update an address ================
+@address_routes.route('/<int:address_id>', methods=['PUT'])
+@login_required
+def user_update_address(address_id):
+    address = Address.query.get(address_id)
+    if not address:
+        return {
+            "message": "Address couldn't found.",
+            "status_code": 404
+        }, 404
+    else:
+        form = AddressForm()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        address.address_line1 = form.data['address_line1']
+        address.address_line2 = form.data['address_line2']
+        address.unit_number = form.data['unit_number']
+        address.city = form.data['city']
+        address.state = form.data['state']
+        address.zip_code = form.data['zip_code']
+
+        db.session.commit()
+        return jsonify(address.to_dict())
 
 # =========== Delete an address ==============
-# ========= how to verify address was created by user? ==========
 @address_routes.route('/<int:address_id>', methods=['DELETE'])
 @login_required
 def delete_address(address_id):
