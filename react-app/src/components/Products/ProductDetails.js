f203eb22b584b70cd1a62d46611dae9da5295529
yifan <iffy713@gmail.com>
2022-12-10 19:52:20 -0500

    add product into shopping cart work

--- react-app/src/components/Products/ProductDetails.js
+++ react-app/src/components/Products/ProductDetails.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { thunkAddToCart } from '../../store/cart';
 import { thunkGetSingleProduct } from '../../store/product';
 
 export default function ProductDetails() {
@@ -8,11 +9,17 @@ export default function ProductDetails() {
     console.log("CAN YOU SEE ME")
     const dispatch = useDispatch()
     const singleProduct = useSelector(state => state.product.singleProduct)
-    console.log(singleProduct)
+    // console.log(singleProduct)
     const { productId } = useParams()
+    const [ quantity, setQuantity ] = useState(1)
     const [ loaded, setLoaded ] = useState(false)
     let productImages
 
+    // set quantity selections
+    let options = []
+    for(let i = 1; i<=50; i++){
+        options.push(i)
+    }
 
     useEffect(()=> {
         dispatch(thunkGetSingleProduct(productId))
@@ -22,7 +29,12 @@ export default function ProductDetails() {
     if (loaded) {
         productImages = Object.values(singleProduct.ProductImages)
     }
-    console.log(productImages)
+
+    const handleAddToCart = async () => {
+        await dispatch(thunkAddToCart(productId,quantity))
+        console.log(quantity)
+    }
+
 
 
     return (
@@ -33,9 +45,16 @@ export default function ProductDetails() {
                 {singleProduct.brand_story}
                 {singleProduct.price}
                 {productImages.map(img => (
-                    <div>{img.url}</div>
+                    <img key={img.id} src={img.url} alt={img.id}/>
                 ))}
-                <button>Add to cart</button>
+                <div>
+                    <select value={quantity} onChange={e=>setQuantity(e.target.value)}>
+                        {options.map(option => (
+                            <option key={option}>{option}</option>
+                        ))}
+                    </select>
+                </div>
+                <button onClick={handleAddToCart}>Add to cart</button>
             </div>
 
         )
--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -1,8 +1,15 @@
 const GET_CART_ITEMS = "cart/GET_CART_ITEMS"
+const ADD_TO_CART = "cart/ADD_TO_CART"
+const REMOVE_FROM_CART = "cart/REMOVE_FROM_CART"
 
 const actionGetCartItems = (items) => ({
     type: GET_CART_ITEMS,
     items
+})
+
+const actionAddToCart = (item) => ({
+    type: ADD_TO_CART,
+    item
 })
 
 export const thunkGetCartItems = () => async (dispatch)=>{
@@ -14,6 +21,21 @@ export const thunkGetCartItems = () => async (dispatch)=>{
     }
 }
 
+export const thunkAddToCart = (id, quantity) => async (dispatch) => {
+    const response = await fetch(`/api/products/${id}/cart`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({quantity})
+    })
+    if(response.ok) {
+        const data = await response.json()
+        dispatch(actionAddToCart(data))
+        return data
+    }
+}
+
 const cartReducer = ( state={}, action ) => {
     let newState = {}
     switch ( action.type ) {
@@ -22,6 +44,14 @@ const cartReducer = ( state={}, action ) => {
                 newState[item.id] = item
             })
             return newState
+
+        case ADD_TO_CART:
+            newState={
+                ...newState,
+            }
+            newState[action.item.id] = action.item
+            return newState
+
         default:
             return state
     }
