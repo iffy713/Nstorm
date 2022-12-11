d1a667c03646596bfc2e03737ddbdd3617c9238f
yifan <iffy713@gmail.com>
2022-12-11 00:56:20 -0500

    remove cart items work well

--- react-app/src/components/ShoppingBag/ShoppingBag.js
+++ react-app/src/components/ShoppingBag/ShoppingBag.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { thunkGetCartItems } from "../../store/cart";
+import { thunkGetCartItems, thunkRemoveCartItem } from "../../store/cart";
 
 export default function ShoppingBag(){
 
     const dispatch = useDispatch()
     const cartItemsObj = useSelector(state=> state.cartItems)
     const cartItemsArr = Object.values(cartItemsObj)
-    const products = useSelector(state => state.product.allProducts)
+    // const products = useSelector(state => state.product.allProducts)
+    const [ quantity, setQuantity ] = useState()
+
+    // set quantity selections
+    let options = []
+    for(let i = 1; i<=50; i++){
+        options.push(i)
+    }
 
     useEffect(()=>{
         dispatch(thunkGetCartItems())
@@ -17,8 +24,19 @@ export default function ShoppingBag(){
         <div>
             shopping bag component
             {cartItemsArr.map(item => (
-                <div key={item.id}>{item.id} quantity {item.quantity}</div>
+                <div key={item.id}>
+                    {item.id} quantity {item.quantity}
+                    <select value={item.quantity} onChange={e => setQuantity(e.target.value)}>
+                    {options.map(option => (
+                        <option key={option}>{option}</option>
+                    ))}
+                    </select>
+                    <button onClick={()=>dispatch(thunkRemoveCartItem(item.id))}>Remove</button>
+                </div>
             ))}
+            <div>
+
+            </div>
         </div>
     )
 }
--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -1,5 +1,6 @@
 const GET_CART_ITEMS = "cart/GET_CART_ITEMS"
 const ADD_TO_CART = "cart/ADD_TO_CART"
+const UPDATE_ITEM_QUANTITY = "cart/UPDATE_ITEM_QUANTITY"
 const REMOVE_FROM_CART = "cart/REMOVE_FROM_CART"
 
 const actionGetCartItems = (items) => ({
@@ -10,6 +11,17 @@ const actionGetCartItems = (items) => ({
 const actionAddToCart = (item) => ({
     type: ADD_TO_CART,
     item
+})
+
+const actionUpdateItemQuantity = (itemId, quantity) => ({
+    type: UPDATE_ITEM_QUANTITY,
+    itemId,
+    quantity
+})
+
+const actionRemoveCartItem = (itemId) => ({
+    type: REMOVE_FROM_CART,
+    itemId
 })
 
 export const thunkGetCartItems = () => async (dispatch)=>{
@@ -36,6 +48,38 @@ export const thunkAddToCart = (id, quantity) => async (dispatch) => {
     }
 }
 
+export const thunkUpdateItemQuantity = (item_id, quantity) => async (dispatch) => {
+    const response = await fetch( `/api/cart/${item_id}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({quantity})
+    })
+    if(response.ok) {
+        const data = await response.json()
+        dispatch(actionUpdateItemQuantity(item_id, quantity))
+        return data
+    } else {
+        const data = await response.json()
+        return data.errors
+    }
+}
+
+export const thunkRemoveCartItem = (item_id) => async (dispatch) => {
+    const response = await fetch(`/api/cart/${item_id}`, {
+        method: "DELETE"
+    })
+    if(response.ok) {
+        const data = await response.json()
+        dispatch(actionRemoveCartItem(item_id))
+        return data
+    } else {
+        const data = await response.json()
+        return data.errors
+    }
+}
+
 const cartReducer = ( state={}, action ) => {
     let newState = {}
     switch ( action.type ) {
@@ -47,9 +91,23 @@ const cartReducer = ( state={}, action ) => {
 
         case ADD_TO_CART:
             newState={
-                ...newState,
+                ...state,
             }
             newState[action.item.id] = action.item
+            return newState
+
+        case UPDATE_ITEM_QUANTITY:
+            newState = {
+                ...state
+            }
+            newState[action.itemId].quantity = action.quantity
+            return newState
+
+        case REMOVE_FROM_CART:
+            newState = {
+                ...state
+            }
+            delete newState[action.itemId]
             return newState
 
         default:
