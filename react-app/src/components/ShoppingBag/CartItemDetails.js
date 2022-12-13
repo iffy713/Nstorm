543fff0f87239c0588fc61df3eb97243b2403dd7
yifan <iffy713@gmail.com>
2022-12-13 13:54:24 -0500

    update cart item quantity work

--- react-app/src/components/ShoppingBag/CartItemDetails.js
+++ react-app/src/components/ShoppingBag/CartItemDetails.js
@@ -1,18 +1,41 @@
 import React, { useEffect, useState } from "react";
-
-
+import { useDispatch } from "react-redux";
+import { thunkUpdateItemQuantity } from "../../store/cart";
 
 export default function CartItemDetails({item}){
 
-    console.log(item)
-    // const productObj = useState(state => state.product.singleProduct)
+    const dispatch = useDispatch()
+    const [ loaded, setLoaded ] = useState(false)
+    const [ quantity, setQuantity ] = useState(item.quantity)
+    const [ itemPrice, setItemPrice ] = useState(quantity * item.Product.price)
+
+
+    useEffect(()=>{
+        setLoaded(true)
+        dispatch(thunkUpdateItemQuantity(item.id, quantity))
+            .then(setItemPrice(quantity * item.Product.price))
+
+    }, [dispatch, quantity])
+
+    let options = []
+    for(let i = 1; i<=50; i++){
+        options.push(i)
+    }
 
     return (
-        <div>
-            Cart item details
-            <br></br>
-            {item.id} quantity {item.quantity}
-
-        </div>
+        loaded && (
+            <div>
+                Cart item details
+                <br></br>
+                {item.Product.name} quantity {item.quantity}
+                <select value={quantity} onChange={(e)=>setQuantity(e.target.value)}>
+                    {options.map(option => (
+                        <option>{option}</option>
+                    ))}
+                </select>
+                ${item.Product.price}
+                total ${itemPrice}
+            </div>
+        )
     )
 }
