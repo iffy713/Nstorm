16e41e2d4a00e813e087e45fcb772d471a7c1a24
yifan <iffy713@gmail.com>
2022-12-21 01:59:14 -0500

    remove rating line from code, then merge to main

--- react-app/src/components/CreateReviewFormModal/CreateReviewForm.js
+++ react-app/src/components/CreateReviewFormModal/CreateReviewForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { thunkCreateReview } from "../../store/review"
-import { StarRatings } from  'react-star-ratings'
+
 
 export default function CreateReviewForm({setShowModal, productId}){
 
