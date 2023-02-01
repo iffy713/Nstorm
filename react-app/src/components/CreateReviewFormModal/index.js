9b42367ae074acdd1ac0630368ce9e76d91feca6
yifan <iffy713@gmail.com>
2023-02-01 12:20:16 -0500

    review for unloged user bug fixed

--- react-app/src/components/CreateReviewFormModal/index.js
+++ react-app/src/components/CreateReviewFormModal/index.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Modal } from '../../context/Modal'
+import { Link, useHistory } from "react-router-dom";
 import CreateReviewForm from "./CreateReviewForm";
 
 export default function CreateReviewFormModal({productId}) {
     const [ showModal, setShowModal ] = useState(false)
+    const currUser = useSelector(state => state.session.user)
+    const history = useHistory()
+
+    const handleClick = () => {
+        if(!currUser){
+            history.push('/login')
+        }
+        else{
+            setShowModal(true)
+        }
+    }
 
     return (
         <>
-            <button onClick={()=> setShowModal(true)}>Write a Review</button>
+            <button onClick={handleClick}>Write a Review</button>
             { showModal && (
                 <Modal onClose={()=>setShowModal(false)}>
                     <CreateReviewForm setShowModal={setShowModal} productId={productId}/>
--- react-app/src/components/Reviews/Reviews.js
+++ react-app/src/components/Reviews/Reviews.js
@@ -12,10 +12,15 @@ export default function Reviews({productId}) {
     const dispatch = useDispatch()
     const productReviewsObj = useSelector(state => state.review)
     const productReviewsArr = Object.values(productReviewsObj)
-    const currUserId = useSelector(state => state.session.user.id)
+
+    let userId
+    const currUser = useSelector(state => state.session.user)
+    if(currUser){
+        userId = currUser.id
+    }
     // const userReview = productReviewsArr.filter(review => review.User.id === currUserId)
     // console.log("user review", userReview)
-    const userReview = productReviewsArr.find(review => review.user_id === currUserId)
+    const userReview = productReviewsArr.find(review => review.user_id === userId)
     // console.log(userReview)
 
     useEffect(()=>{
