ccb726139097561f3c8eb751232e6b188e9609bc
yifan <iffy713@gmail.com>
2023-02-01 20:13:49 -0500

    css for reviews done

--- react-app/src/components/CreateReviewFormModal/CreateReviewForm.js
+++ react-app/src/components/CreateReviewFormModal/CreateReviewForm.js
@@ -1,15 +1,15 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { thunkAddReviewImage, thunkCreateReview, thunkGetProductReviews } from "../../store/review"
-// import AddImage from "./AddImage"
+import StarRating from 'react-star-ratings'
 import "./CreateReview.css"
 
 
-export default function CreateReviewForm({setShowModal, productId}){
+export default function CreateReviewForm({setShowModal, productId, product}){
 
     const dispatch = useDispatch()
     const [ headline, setHeadline ] = useState("")
-    const [ stars, setStars ] = useState(5)
+    const [ stars, setStars ] = useState(0)
     const [ review, setReview ] = useState("")
     const [ errors, setErrors ] = useState([])
 
@@ -37,26 +37,6 @@ export default function CreateReviewForm({setShowModal, productId}){
                 }
             })
         }
-
-        // await dispatch(thunkAddReviewImage(newReview.id, image)).then(async(error) =>{
-        //     await dispatch(thunkGetProductReviews(productId))
-        //     if (error){
-        //         setErrors(error)
-        //     } else {
-        //         setShowModal(false)
-        //     }
-        // })
-
-        // await dispatch(thunkCreateReview(productId, stars, headline, review)).then(async(error)=>{
-
-        //     await dispatch(thunkGetProductReviews(productId))
-
-        //     if (error) {
-        //         setErrors(error)
-        //     } else {
-        //         setShowModal(false)
-        //     }
-        // })
     }
 
     // =====================================
@@ -82,39 +62,73 @@ export default function CreateReviewForm({setShowModal, productId}){
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <h2>
+                Write a review
+            </h2>
+            <div>{product.name}</div>
+            <form onSubmit={handleSubmit} className="review-form">
                 <div>
                     {errors.map((error, ind) => (
                         <div key={ind}>{error}</div>
                     ))}
                 </div>
+                <div className='form-label-ctn'>
+                    <label htmlFor="rating">Rating<span className='red-star-span'>*</span></label>
+                </div>
                 <div>
-                    <label htmlFor="rating">Rating</label>
-                    <select value={stars} onChange={e => setStars(e.target.value)}>
+                    {/* <select value={stars} onChange={e => setStars(e.target.value)}>
                         {rating.map(rate=>(
                             <option key={rate}
                                 name={"rating"}
                             >{rate}</option>
                         ))}
-                    </select>
+                    </select> */}
+                    <StarRating
+                        isSelectable={true}
+                        rating={stars}
+                        changeRating={(rating) => setStars(rating)}
+                        numberOfStars={5}
+                        starHoverColor="rgb(57,57,57)"
+                        starRatedColor="rgb(57, 57, 57)"
+                        starEmptyColor="rgb(227, 227, 227)"
+                        starDimension='18px'
+                        starSpacing='2px'
+                    />
+                </div>
+
+                <div className='form-label-ctn'>
+                    <label htmlFor="fileInput" >
+                        <button
+                            class="file-button"
+                            onClick={() => document.getElementById("fileInput").click()}
+                        >
+                        <i class="fa-solid fa-camera"></i> <span>
+                            Add a photo
+                        </span>
+                        </button>
+                    </label>
                 </div>
                 <div>
                     <input
-                        name="images"
+                        name="fileInput"
                         type="file"
                         id="fileInput"
                         accept="image/*"
                         onChange={handleChange}
                         multiple
                     />
-                    <div id="upload-image-ctn">
-                        {preview.map((item, index)=> (
-                            <img key={index} src={item} alt="preview" id="preview-review-image"/>
-                        ))}
-                    </div>
+                    {preview.length?(
+                        <div id="upload-image-ctn">
+                            {preview.map((item, index)=> (
+                                <img key={index} src={item} alt="preview" id="preview-review-image"/>
+                            ))}
+                        </div>
+                    ):(<div></div>)}
+                </div>
+                <div className='form-label-ctn'>
+                    <label htmlFor="headline">Headline<span className='red-star-span'>*</span></label>
                 </div>
                 <div>
-                    <label htmlFor="headline">Headline*</label>
                     <input
                         name="headline"
                         type="text"
@@ -122,8 +136,10 @@ export default function CreateReviewForm({setShowModal, productId}){
                         onChange={e => setHeadline(e.target.value)}
                         placeholder="Summarize your review in a few words." ></input>
                 </div>
+                <div className='form-label-ctn'>
+                    <label htmlFor="review">Review<span className='red-star-span'>*</span></label>
+                </div>
                 <div>
-                    <label htmlFor="review">Review</label>
                     <textarea
                         name="review"
                         value={review}
@@ -131,8 +147,8 @@ export default function CreateReviewForm({setShowModal, productId}){
                         placeholder="Write about what you did or didn't like about this product. Imclude details that would be helpful to other shoppers." />
                 </div>
                 <div>
-                    <button type="submit">Submit</button>
-                    <button onClick={()=>setShowModal(false)}>Cancel</button>
+                    <button type="submit" id="submit-review-btn">Submit</button>
+                    <button onClick={()=>setShowModal(false)} id='cancel-review-btn'>Cancel</button>
                 </div>
             </form>
         </div>
