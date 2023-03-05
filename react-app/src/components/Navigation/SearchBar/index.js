f3c7e8c099062322b028de4550d6183048ea9156
yifan <iffy713@gmail.com>
2023-03-04 23:35:30 -0500

    search function added

--- react-app/src/components/Navigation/SearchBar/index.js
+++ react-app/src/components/Navigation/SearchBar/index.js
@@ -1,14 +1,36 @@
+import { useState } from 'react'
 import './index.css'
+
 export default function SearchBar() {
+
+    const [ keyword, setKeyword ] = useState("")
+
+    // const handleKeyPress = (e) => {
+    //     if(e.key === 'Enter'){
+    //         handleSearch()
+    //     }
+    // }
+
+    // const handleSearch = (e) => {
+    //     e.preventDefault()
+
+    //     console.log(keyword)
+    // }
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSearch}>
+                <input id='search-bar-hidden-input'/>
                 <div id='search-bar-ctn'>
                     <div id='search-icon-ctn'>
                         <i className="fa-solid fa-magnifying-glass"></i>
                     </div>
-                    <input placeholder="Search for products or brands" />
+                    <input id='key-word-input'
+                        value={keyword}
+                        onChange={(e)=> setKeyword(e.target.value)}
+                        maxLength='80'
+                        onKeyPress={handleKeyPress}
+                        placeholder="Search for products or brands" />
                 </div>
             </form>
         </div>
